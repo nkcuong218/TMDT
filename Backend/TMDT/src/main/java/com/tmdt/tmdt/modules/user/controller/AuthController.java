85a081f961f14e072015d6eb7928cf1ad1ebb426
ngkhcuong <ngkhcuong218@gmail.com>
2026-03-12 07:57:33 +0700

    cors config

--- Backend/TMDT/src/main/java/com/tmdt/tmdt/modules/user/controller/AuthController.java
+++ Backend/TMDT/src/main/java/com/tmdt/tmdt/modules/user/controller/AuthController.java
@@ -14,7 +14,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(
+        origins = "http://localhost:5173",
+        allowedHeaders = "*",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS}
+)
 public class AuthController {
 
     private final AuthService authService;
